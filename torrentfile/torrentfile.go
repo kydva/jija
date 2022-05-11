8f8bd91e2319f049ab306aeabab638ea52097fb4
Vladyslav Yavdyk <vladyslav.yavdyk@folderly.com>
2022-05-11 18:45:56 +0300

    Split pieces, add infohash

--- torrentfile/torrentfile.go
+++ torrentfile/torrentfile.go
@@ -1,32 +1,101 @@
 package torrentfile
 
 import (
+	"bytes"
+	"crypto/sha1"
 	"os"
 
 	"github.com/jackpal/bencode-go"
 )
 
-type TorrentInfo struct {
+const HASH_LENGTH = 20
+
+type hash [HASH_LENGTH]byte
+
+type rawInfo struct {
 	Pieces      string
 	PieceLength int `bencode:"piece length"`
 	Length      int
 	Name        string
 }
 
-type TorrentFile struct {
+type rawTorrentFile struct {
 	Announce string
-	Info     TorrentInfo
+	Info     rawInfo
+}
+
+type TorrentFile struct {
+	Announce    string
+	InfoHash    hash
+	Pieces      []hash
+	PieceLength int
+	Length      int
+	Name        string
 }
 
 func Open(path string) (TorrentFile, error) {
 	file, err := os.Open(path)
-	decoded := TorrentFile{}
 	if err != nil {
-		return decoded, err
+		return TorrentFile{}, err
 	}
-	err = bencode.Unmarshal(file, &decoded)
+	decoded, err := decode(file)
 	if err != nil {
-		return decoded, err
+		return TorrentFile{}, err
 	}
-	return decoded, nil
+	return decoded.toTorrentFile()
+}
+
+func decode(file *os.File) (rawTorrentFile, error) {
+	data := rawTorrentFile{}
+	err := bencode.Unmarshal(file, &data)
+	if err != nil {
+		return data, err
+	}
+	return data, nil
+}
+
+func (info *rawInfo) hash() (hash hash, err error) {
+	var buf bytes.Buffer
+	err = bencode.Marshal(&buf, *info)
+	if err != nil {
+		return [20]byte{}, err
+	}
+	hash = sha1.Sum(buf.Bytes())
+	return hash, nil
+}
+
+func (info *rawInfo) splitPieces() ([]hash, error) {
+	buf := []byte(info.Pieces)
+	piecesCount := len(buf) / HASH_LENGTH
+	pieces := make([]hash, piecesCount)
+
+	for i := 0; i < piecesCount; i++ {
+		start := i * HASH_LENGTH
+		end := (i + 1) * HASH_LENGTH
+		copy(pieces[i][:], buf[start:end])
+	}
+	return pieces, nil
+}
+
+func (file *rawTorrentFile) toTorrentFile() (TorrentFile, error) {
+	infoHash, err := file.Info.hash()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+
+	pieces, err := file.Info.splitPieces()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+
+	torrentFile := TorrentFile{
+		Announce:    file.Announce,
+		InfoHash:    infoHash,
+		Pieces:      pieces,
+		PieceLength: file.Info.PieceLength,
+		Length:      file.Info.Length,
+		Name:        file.Info.Name,
+	}
+
+	return torrentFile, nil
 }
--- torrentfile/torrentfile_test.go
+++ torrentfile/torrentfile_test.go
@@ -1,30 +1,36 @@
 package torrentfile
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestOpen(t *testing.T) {
-	file, err := Open("testdata/file.torrent")
+	data, err := Open("testdata/file.torrent")
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	expected := TorrentFile{
-		Announce: "udp://tracker.openbittorrent.com:80",
-		Info: TorrentInfo{
-			PieceLength: 65536,
-			Length:      20,
-		},
+		Announce:    "udp://tracker.opentrackr.org:1337/announce",
+		PieceLength: 262144,
+		Length:      363548672,
 	}
 
-	if file.Announce != expected.Announce {
-		t.Fatalf("Announce %s != %s", file.Announce, expected.Announce)
+	if data.Announce != expected.Announce {
+		t.Fatalf("Announce %s != %s", data.Announce, expected.Announce)
 	}
 
-	if file.Info.Length != expected.Info.Length {
-		t.Fatalf("Info.Length %d != %d", file.Info.Length, expected.Info.Length)
+	if data.Length != expected.Length {
+		t.Fatalf("Length %d != %d", data.Length, expected.Length)
 	}
 
-	if file.Info.PieceLength != expected.Info.PieceLength {
-		t.Fatalf("Info.PieceLength %d != %d", file.Info.PieceLength, expected.Info.PieceLength)
+	if data.PieceLength != expected.PieceLength {
+		t.Fatalf("PieceLength %d != %d", data.PieceLength, expected.PieceLength)
+	}
+
+	fifthPieceHash := hash{46, 0, 15, 167, 232, 87, 89, 199, 244, 194, 84, 212, 217, 195, 62, 244, 129, 228, 89, 167}
+
+	if data.Pieces[5] != fifthPieceHash {
+		t.Fatalf("5th piece hash %x != %x", data.Pieces[5], fifthPieceHash)
 	}
 }
